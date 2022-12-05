96e568731c2e3630e63f33ec322db02f091285f7
Daniel Roncancio <daniel@roncancio.co>
2022-12-05 00:19:20 -0500

    First day solution

--- src/bin/day1_1.rs
+++ src/bin/day1_1.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello fuckers!");
+    let result: i32 = include_str!("../../inputs/day1.txt")
+        .split("\n\n")
+        .map(|x| x.lines().flat_map(str::parse::<i32>).sum())
+        .max()
+        .unwrap();
+
+    println!("{}", result); // 70116
 }
--- src/bin/day1_2.rs
+++ src/bin/day1_2.rs
@@ -1,4 +1,12 @@
 fn main() {
-    let test_file = include_str!("../../inputs/day1_1.txt");
-    println!("{}", test_file);
+    let mut values : Vec<i32> = include_str!("../../inputs/day1.txt")
+        .split("\n\n")
+        .map(|x| x.lines().flat_map(str::parse::<i32>).sum())
+        .collect();
+
+   values.sort_by(|a, b| b.cmp(a));
+
+    let result: i32 = values.iter().take(3).sum();
+
+    println!("{:?}", result); // 206582
 }
